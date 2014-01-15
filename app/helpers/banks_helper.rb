4b9706ee569e3914a553ecbf639a2663956e08d0
Temo Chutlashvili <temo.chutlashvili@telasi.ge>
2014-01-15 16:08:46 +0400

    Banks correction

--- app/helpers/banks_helper.rb
+++ app/helpers/banks_helper.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 module BanksHelper
-  def banks; Hash[Bank::BANKS.map{|k,v| ["#{k} &mdash; #{v}".html_safe,k]}] end
+  def banks; Hash[Bank::BANKS.map{|k,v| ["#{k} &mdash; #{ t("banks.#{k}") }".html_safe,k]}] end
 end
