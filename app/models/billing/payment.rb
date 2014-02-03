eed67fd8c936a03fa297b42acc1e7487400f9661
Temo Chutlashvili <temo.chutlashvili@telasi.ge>
2014-02-03 16:58:18 +0400

    Payment, added set_integer_columns

--- app/models/billing/payment.rb
+++ app/models/billing/payment.rb
@@ -5,5 +5,6 @@ class Billing::Payment < ActiveRecord::Base
   self.table_name  = 'payments.payment'
   self.primary_key = :paykey
   self.sequence_name = 'payments.payments_seq'
+  self.set_integer_columns :status
   belongs_to :customer, class_name: 'Billing::Customer', foreign_key: :custkey
 end
