e9e1925422f05e01cccf335c2af681a0a0296950
Dimitri Kurashvili <dimakura@gmail.com>
2014-02-07 14:25:50 +0400

    application routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # -*- encoding : utf-8 -*-
 module ApplicationHelper
+  def routes
+    (Rails.application.routes.routes.select {|r| r.defaults[:controller]}).map {|r| {controller: r.defaults[:controller], action: r.defaults[:action], path: r.path.spec.to_s}}
+  end
 end
