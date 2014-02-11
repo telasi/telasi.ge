6c9c9768ece0cdc5e3e76ce70ad8731d0519bcff
Dimitri Kurashvili <dimakura@gmail.com>
2014-02-11 10:36:21 +0400

    open permissions

--- app/models/sys/permission.rb
+++ app/models/sys/permission.rb
@@ -23,25 +23,26 @@ class Sys::Permission
   end
 
   def self.has_permission?(user, controller, action)
-    if user and user.admin? then true # admin has all privileges
-    elsif Sys::Permission.count == 0 then true # no permissions defined yet
-    else
-      permission = Sys::Permission.where(controller: controller, action: action).first
-      if permission
-        if permission.public_page then true # everyone has access to public page
-        elsif user.blank? then false # authentication required for non-public pages
-        elsif permission.admin_page then false # only admin has access to admin page
-        else
-          if permission.roles.empty? then true # no role required for this action
-          else
-            if (permissin.roles & user.roles).any? then true # user roles has intersection with permission roles
-            else false end # ... no intersection => no permission
-          end
-        end
-      else
-        false
-      end
-    end
+    # if user and user.admin? then true # admin has all privileges
+    # elsif Sys::Permission.count == 0 then true # no permissions defined yet
+    # else
+    #   permission = Sys::Permission.where(controller: controller, action: action).first
+    #   if permission
+    #     if permission.public_page then true # everyone has access to public page
+    #     elsif user.blank? then false # authentication required for non-public pages
+    #     elsif permission.admin_page then false # only admin has access to admin page
+    #     else
+    #       if permission.roles.empty? then true # no role required for this action
+    #       else
+    #         if (permissin.roles & user.roles).any? then true # user roles has intersection with permission roles
+    #         else false end # ... no intersection => no permission
+    #       end
+    #     end
+    #   else
+    #     false
+    #   end
+    # end
+    true
   end
 
   def roles2
