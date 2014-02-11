2079c8bd7b39a53afb3f72964e871728fcd6989a
Dimitri Kurashvili <dimakura@gmail.com>
2014-02-11 11:53:14 +0400

    adding permissions

--- config/initializers/telasi.rb
+++ config/initializers/telasi.rb
@@ -3,4 +3,9 @@ module Telasi
   HOST = "http://my.telasi.ge"
   MAILGUN_KEY = "key-20k5k0urfnewhu8a6of2to26uscsm-71"
   NEWCUSTAPP_UNDERCONSTRUCTION = true
+  PERMISSIONS = {
+    network_admin_id:       '52f4c9493bd0417158000004',
+    network_cancelaria_id:  '52f4c91a3bd04103d4000003',
+    subscriptions_admin_id: '52f890373bd041079e000002',
+  }
 end
