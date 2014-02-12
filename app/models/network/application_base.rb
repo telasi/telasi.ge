ced5b9562a0973dc7c5e7ef35cee4221e026264d
Dimitri Kurashvili <dimakura@gmail.com>
2014-02-12 12:15:19 +0400

    adding regions to network application
    based on requirements from Rusiko and Medea:
    we added region to
    Network::NewCustomerApplication
    Network::ChangePowerApplication

--- app/models/network/application_base.rb
+++ app/models/network/application_base.rb
@@ -4,7 +4,10 @@ module Network::ApplicationBase
   BASE_DOC  = 'document'
 
   def self.included(base)
-    base.field :base_type, type: String, default: Network::ApplicationBase::BASE_NONE
+    base.field :base_type,   type: String, default: Network::ApplicationBase::BASE_NONE
     base.field :base_number, type: String
+    base.field :region,      type: String
   end
+
+  def self.regions; I18n.t('models.application_base.regions').split(',') end
 end
