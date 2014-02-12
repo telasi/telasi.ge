a4646ef607974f2a038d5aa6156bba86a51a2f06
Dimitri Kurashvili <dimakura@gmail.com>
2014-02-12 14:09:42 +0400

    fixing rs_name
    we should query for rs_name even if rs_tin not changed
    but rs_name is empty

--- app/models/network/rs_name.rb
+++ app/models/network/rs_name.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 module Network::RsName
   def validate_rs_name
-    if self.rs_tin.present? and self.rs_tin_changed?
+    if self.rs_tin.present? and (self.rs_tin_changed? or self.rs_name.blank?)
       unless self.rs_foreigner
         self.rs_name = RS.get_name_from_tin(RS::TELASI_SU.merge(tin: self.rs_tin)) #if self.rs_name.blank?
         if self.rs_name.blank?
