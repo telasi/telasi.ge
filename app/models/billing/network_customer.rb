a58b72b975d9ba5f6a0b35d6fd11a781aa730642
Dimitri Kurashvili <dimakura@gmail.com>
2014-02-27 00:02:44 -0800

    Update network_customer.rb

--- app/models/billing/network_customer.rb
+++ app/models/billing/network_customer.rb
@@ -4,4 +4,16 @@ class Billing::NetworkCustomer < ActiveRecord::Base
   self.primary_key = 'zdepozit_cust_id'
 
   belongs_to :customer, class_name: 'Billing::Customer', foreign_key: :custkey
+
+  def self.from_bs_customer(customer)
+    Billing::NetworkCustomer.new(
+      customer: customer,
+      custname: customer.custname,
+      taxid_or_personalid: customer.taxid,
+      status: 0,
+      perskey: 1,
+      is_printed: 0,
+      debet_type: 1
+    ).save
+  end
 end
