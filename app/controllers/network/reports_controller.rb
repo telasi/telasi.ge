af06fdca1cae49e967c888fa2b05ff777a7e2d5b
Dimitri Kurashvili <dimakura@gmail.com>
2014-03-07 15:24:51 +0400

    adding second report for rusiko: complete apps status

--- app/controllers/network/reports_controller.rb
+++ app/controllers/network/reports_controller.rb
@@ -12,6 +12,14 @@ class Network::ReportsController < ApplicationController
     @chgapps = Network::ChangePowerApplication.where(:send_date.gte => @d1, :send_date.lte => @d2)
   end
 
+  def completed_apps
+    @title = 'ქსელზე მიერთების შესრულებები'
+    @d1 = Date.strptime(params[:d1], '%d-%b-%Y') rescue (Date.today - 30)
+    @d2 = Date.strptime(params[:d2], '%d-%b-%Y') rescue Date.today
+    stats = [Network::NewCustomerApplication::STATUS_COMPLETE, Network::NewCustomerApplication::STATUS_IN_BS]
+    @newapps = Network::NewCustomerApplication.where(:send_date.gte => @d1, :send_date.lte => @d2, :status.in => stats)
+  end
+
   def nav
     @nav = { 'ქსელი' => network_home_url, 'რეპორტები' => network_reports_url }
     @nav[@title] = nil unless ['index'].include?(action_name )
