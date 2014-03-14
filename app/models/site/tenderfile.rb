c6298307517db010e452948800981f31a5e574d8
temochutlashvili <temo.chutlashvili@telasi.ge>
2014-03-14 17:16:49 +0400

    Tender: update fileexist checkbox for all languages

--- app/models/site/tenderfile.rb
+++ app/models/site/tenderfile.rb
@@ -8,11 +8,11 @@ class Site::Tenderfile < ActiveRecord::Base
   private
 
   def before_save
-  	@tenderrevision = Site::Tenderfilerevision.where(entity_type: self.entity_type, deleted: self.deleted, entity_id: self.entity_id, language: self.language, delta: self.delta).first || Site::Tenderfilerevision.new(entity_type: self.entity_type, bundle: self.bundle, deleted: self.deleted, entity_id: self.entity_id, revision_id: self.revision_id, language: self.language, delta: self.delta)
-   	@tenderrevision.field_fileexists_value = self.field_fileexists_value
-	@tenderrevision.save
+    	@tenderrevision = Site::Tenderfilerevision.where(entity_type: self.entity_type, deleted: self.deleted, entity_id: self.entity_id, language: self.language, delta: self.delta).first || Site::Tenderfilerevision.new(entity_type: self.entity_type, bundle: self.bundle, deleted: self.deleted, entity_id: self.entity_id, revision_id: self.revision_id, language: self.language, delta: self.delta)
+     	@tenderrevision.field_fileexists_value = self.field_fileexists_value
+  	  @tenderrevision.save
 
-	@cache = Site::Cachefield.where(cid: "field:node:#{self.entity_id}").first
-	@cache.destroy if @cache
+  	  @cache = Site::Cachefield.where(cid: "field:node:#{self.entity_id}").first
+  	  @cache.destroy if @cache
   end
 end
--- app/models/tender/tender.rb
+++ app/models/tender/tender.rb
@@ -13,16 +13,27 @@ class Tender::Tender
   private
 
    def aftersave
-   	@tender = Site::Tenderfile.where(entity_type: 'node', deleted: 0, entity_id: self.nid, language: 'und', delta: 0).first || Site::Tenderfile.new(entity_type: 'node', bundle: 'news', deleted: 0, revision_id: self.nid, entity_id: self.nid, language: 'und', delta: 0)
-   	@tender.field_fileexists_value = 1
-	  @tender.save
+    @tenders = Site::Node.where(tnid: self.nid)
+
+    @tenders.each do |t|
+
+     	@tender = Site::Tenderfile.where(entity_type: 'node', deleted: 0, entity_id: t.nid, language: 'und', delta: 0).first || Site::Tenderfile.new(entity_type: 'node', bundle: 'news', deleted: 0, revision_id: t.nid, entity_id: t.nid, language: 'und', delta: 0)
+     	@tender.field_fileexists_value = 1
+  	  @tender.save
+
+    end
    end
 
    def beforedestroy
-   	@tender = Site::Tenderfile.where(entity_type: 'node', deleted: 0, entity_id: self.nid, language: 'und', delta: 0).first
-   	if @tender 
-   	  @tender.field_fileexists_value = 0
-	    @tender.save
+    @tenders = Site::Node.where(tnid: self.nid)
+
+    @tenders.each do |t|
+
+     	@tender = Site::Tenderfile.where(entity_type: 'node', deleted: 0, entity_id: self.nid, language: 'und', delta: 0).first
+     	if @tender 
+     	  @tender.field_fileexists_value = 0
+  	    @tender.save
+      end
     end
    end
 
