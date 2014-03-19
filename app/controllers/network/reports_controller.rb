e689dd8fa06e4050879361ad81d1df64b6407d0f
Dimitri Kurashvili <dimakura@gmail.com>
2014-03-19 10:40:56 +0400

    change power completeness report

--- app/controllers/network/reports_controller.rb
+++ app/controllers/network/reports_controller.rb
@@ -20,6 +20,14 @@ class Network::ReportsController < ApplicationController
     @newapps = Network::NewCustomerApplication.where(:send_date.gte => @d1, :send_date.lte => @d2, :status.in => stats)
   end
 
+  def completed_apps_change_power
+    @title = 'ქსელში ცლილების შესრულება'
+    @d1 = Date.strptime(params[:d1], '%d-%b-%Y') rescue (Date.today - 30)
+    @d2 = Date.strptime(params[:d2], '%d-%b-%Y') rescue Date.today
+    stats = [Network::ChangePowerApplication::STATUS_COMPLETE, Network::ChangePowerApplication::STATUS_IN_BS]
+    @apps = Network::ChangePowerApplication.where(:send_date.gte => @d1, :send_date.lte => @d2, :status.in => stats)
+  end
+
   def nav
     @nav = { 'ქსელი' => network_home_url, 'რეპორტები' => network_reports_url }
     @nav[@title] = nil unless ['index'].include?(action_name )
