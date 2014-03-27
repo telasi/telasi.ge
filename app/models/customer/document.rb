72889fad61f9f9e12655931b47a824c7db1d81ed
Dimitri Kurashvili <dimakura@gmail.com>
2014-03-27 12:49:46 +0400

    adding statuses to Customer::Registration

--- app/models/customer/document.rb
+++ app/models/customer/document.rb
@@ -3,6 +3,7 @@ class Customer::Document
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  belongs_to :doctype, class_name: 'Customer::DocumentType'
+  belongs_to :document_type, class_name: 'Customer::DocumentType'
+  belongs_to :registation, class_name: 'Customer::Registration'
   field :complete, type: Mongoid::Boolean
 end
--- app/models/customer/registration.rb
+++ app/models/customer/registration.rb
@@ -6,19 +6,22 @@ class Customer::Registration
   include Mongoid::Timestamps
   include Customer::Constants
 
+  STATUS_START = 1
+  STATUS_DOCS_REQUIRED = 2
+  STATUS_COMPLETE = 3
+  STATUS_CANCELED = 4
+
   belongs_to :user, class_name: 'Sys::User'
+  has_many :documents, class_name: 'Customer::Document', as: 'registation'
+  has_many :messages, class_name: 'Sys::SmsMessage', as: 'messageable'
+  field :status, type: Integer, default: STATUS_START
   field :custkey, type: Integer
-
-  field :category, type: String, default: CAT_PERSONAL
-  field :ownership, type: String, default: OWN_OWNER
-
   field :rs_tin, type: String
   field :rs_name, type: String
   field :address, type: String
   field :address_code, type: String
-
-  field :incomplete, type: Mongoid::Boolean, default: false
-  field :confirmed, type: Mongoid::Boolean, default: false
+  field :category, type: String, default: CAT_PERSONAL
+  field :ownership, type: String, default: OWN_OWNER
 
   # validates :custkey, uniqueness: { message: I18n.t('models.customer_registration.errors.customer_duplicate'), scope: :user_id }
   validates :rs_tin, presence: { message: I18n.t('models.customer_registration.errors.tin_required') }
@@ -26,7 +29,27 @@ class Customer::Registration
   validates :address_code, presence: { message: I18n.t('models.customer_registration.errors.address_code_required') }
   validate  :validate_rs_name
 
+  def self.status_name(stat)
+    case stat
+    when STATUS_START then I18n.t('models.customer_registration.status_names.start')
+    when STATUS_DOCS_REQUIRED then I18n.t('models.customer_registration.status_names.docs_required')
+    when STATUS_COMPLETE then I18n.t('models.customer_registration.status_names.complete')
+    when STATUS_CANCELED then I18n.t('models.customer_registration.status_names.canceled')
+    else '?' end
+  end
+
+  def self.status_icon(stat)
+    case stat
+    when STATUS_START then '/icons/clock.png'
+    when STATUS_DOCS_REQUIRED then '/icons/exclamation-red.png'
+    when STATUS_COMPLETE then '/icons/tick.png'
+    when STATUS_CANCELED then '/icons/cross.png'
+    else nil end
+  end
+
   def customer; @customer ||= Billing::Customer.find(self.custkey) end
+  def status_name; Customer::Registration.status_name(self.status) end
+  def status_icon; Customer::Registration.status_icon(self.status) end
 
   private
 
