de36d822f8c08173c10b8d08392811b1c9360951
Dimitri Kurashvili <dimakura@gmail.com>
2014-04-29 10:27:17 +0400

    changes in Bank model

--- app/helpers/banks_helper.rb
+++ app/helpers/banks_helper.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 module BanksHelper
-  def banks; Hash[Bank::BANKS.map{|k,v| ["#{k} &mdash; #{ t("banks.#{k}") }".html_safe,k]}] end
+  def banks; banks=Bank.banks;Hash[banks.map{|k,v| ["#{k} &mdash; #{ t("banks.#{k}") }".html_safe,k]}] end
 end
