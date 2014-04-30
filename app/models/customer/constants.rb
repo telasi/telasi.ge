edd45cacd0f20dc78b8eea47e741cdba6dea4474
Dimitri Kurashvili <dimakura@gmail.com>
2014-04-30 10:35:48 +0400

    russian texts

--- app/models/customer/constants.rb
+++ app/models/customer/constants.rb
@@ -7,8 +7,19 @@ module Customer::Constants
 
   def category_name; Customer::Constants.cat_name(self.category) if self.respond_to?(:category) end
   def ownership_name; Customer::Constants.owner_name(self.ownership) if self.respond_to?(:ownership) end
-  def cat_name(cat); cat == CAT_PERSONAL ? 'ფიზიკური' : 'იურიდიული' end
-  def owner_name(owner); owner == OWN_OWNER ? 'მესაკუთრე' : 'ქირა/იჯარა' end
+
+  def cat_name(cat)
+    cat==CAT_PERSONAL ?
+      I18n.t('models.customer_registration.public_category_personal'):
+      I18n.t('models.customer_registration.public_category_not_personal')
+  end
+
+  def owner_name(owner)
+    owner == OWN_OWNER ?
+      I18n.t('models.customer_registration.ownership_owner'):
+      I18n.t('models.customer_registration.ownership_rent')
+      # 'მესაკუთრე' : 'ქირა/იჯარა'
+  end
 
   module_function :cat_name, :owner_name
 end
