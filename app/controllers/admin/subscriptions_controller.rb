54a0511b373c2a93d0a5687915b01ab76f46770a
Dimitri Kurashvili <dimakura@gmail.com>
2014-07-02 11:50:17 +0400

    headlines page

--- app/controllers/admin/subscriptions_controller.rb
+++ app/controllers/admin/subscriptions_controller.rb
@@ -2,8 +2,12 @@
 class Admin::SubscriptionsController < ApplicationController
   def index
     @title = I18n.t('applications.admin.subscriptions')
+    #@not_sent = Sys::SubscriptionMessage.where(sent: false).asc(:_id)
+  end
+
+  def subscribers
+    @title='სიახლეებზე ხელმოწერები'
     @subscriptions = Sys::Subscription.desc(:_id).paginate(page: params[:page], per_page: 10)
-    @not_sent = Sys::SubscriptionMessage.where(sent: false).asc(:_id)
   end
 
   def headlines
