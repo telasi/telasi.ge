3989968bf9d90ead4624fb729567de3ee1213640
Dimitri Kurashvili <dimakura@gmail.com>
2014-11-11 16:55:34 +0400

    sdweb signature field

--- config/initializers/network.rb
+++ config/initializers/network.rb
@@ -1,4 +1,5 @@
 # -*- encoding : utf-8 -*-
 module Network
   PAYPOINTS = [ 693, 717, 718, 719, 720, 873 ]
+  SDWEB_CMD = 'name=sig1|page=3|type=formfield|subtype=signature|bottom=450|left=200|width=250|height=50'
 end
