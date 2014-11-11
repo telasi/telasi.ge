ec21262ba2bbf628405d8f3bd29999dc530b150f
Dimitri Kurashvili <dimakura@gmail.com>
2014-11-11 18:29:11 +0400

    sd_url

--- config/initializers/network.rb
+++ config/initializers/network.rb
@@ -1,5 +1,6 @@
 # -*- encoding : utf-8 -*-
 module Network
   PAYPOINTS = [ 693, 717, 718, 719, 720, 873 ]
+  SDWEB_URL = 'http://1.1.2.60:8080/sdweb/load/byupload'
   SDWEB_CMD = 'name=sig1|page=3|type=formfield|subtype=signature|bottom=450|left=200|width=250|height=50'
 end
