1558d0c38fe599b5374b795a5e527803d8b38cdb
Dimitri Kurashvili <dimakura@gmail.com>
2015-03-12 17:45:08 +0400

    remove pagination temporarily

--- app/controllers/new_customers_controller.rb
+++ app/controllers/new_customers_controller.rb
@@ -2,7 +2,8 @@
 class NewCustomersController < ApplicationController
   def index
     @title = I18n.t('models.network_new_customer_application.actions.index_page.title')
-    @applications = Network::NewCustomerApplication.where(user: current_user).desc(:_id).paginate(page: params[:page], per_page: 5)
+    #@applications = Network::NewCustomerApplication.where(user: current_user).desc(:_id).paginate(page: params[:page], per_page: 5)
+    @applications = Network::NewCustomerApplication.where(user: current_user).desc(:_id)
   end
 
   def new
