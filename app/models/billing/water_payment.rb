d29421da61af0026c23dc40baa6a297cd2c9cc99
Dimitri Kurashvili <dimakura@gmail.com>
2015-03-13 11:41:48 +0400

    change table schema
    move bs.ruby_water_payments_didube to call.ruby_water_payments_didube

--- app/models/billing/water_payment.rb
+++ app/models/billing/water_payment.rb
@@ -2,7 +2,7 @@
 class Billing::WaterPayment < ActiveRecord::Base
   # self.table_name  = 'paymentsadmin.payments_gwp'
   # self.primary_key = :opayment_id
-  self.table_name  = 'bs.ruby_water_payments_didube'
+  self.table_name  = 'call.ruby_water_payments_didube'
   self.primary_key = :paykey
   belongs_to :customer, class_name: 'Bs::Customer', foreign_key: :custkey
 end
