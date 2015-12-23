28edcc47d7ae77dcfe10aafe5219987c74f4a2b1
Dimitri Kurashvili <dimakura@gmail.com>
2015-12-23 16:01:57 +0400

    new fields in Network::NewCustomerTariff

--- app/models/network/new_customer_tariff.rb
+++ app/models/network/new_customer_tariff.rb
@@ -3,32 +3,21 @@ class Network::NewCustomerTariff
   VOLTAGE_220 = '220'
   VOLTAGE_380 = '380'
   VOLTAGE_610 = '6/10'
+
   include Mongoid::Document
   include Mongoid::Timestamps
+
   field :voltage,    type: String
   field :power_from, type: Integer
   field :power_to,   type: Integer
-  field :days_to_complete, type: Integer
+  field :days_to_complete,                    type: Integer
   field :days_to_complete_without_resolution, type: Integer
-  field :price_gel, type: Float
+  field :days_to_complete_x2,                 type: Integer
+  field :price_gel,  type: Float
+  field :starts, type: Date
+  field :ends,   type: Date
 
-  def self.generate!
-    if Network::NewCustomerTariff.empty?
-      YAML.load_file('data/new-customer-tariffs.yml').values.each do |t|
-        from, to = t['power_kwt'].split('-').map{|p| p.to_i}
-        Network::NewCustomerTariff.new(
-          voltage: t['voltage'],
-          days_to_complete: t['days_to_complete'].to_i,
-          days_to_complete_without_resolution: t['days_to_complete_without_resolution'].to_i,
-          price_gel: t['price_gel'].to_f,
-          power_from: from,
-          power_to: to,
-        ).save
-      end
-    end
-  end
-
-  def self.tariff_for(voltage, power)
+  def self.tariff_for(voltage, power, date=nil)
     Network::NewCustomerTariff.each do |t|
       return t if t.voltage == voltage and power >= t.power_from and power <= t.power_to
     end
