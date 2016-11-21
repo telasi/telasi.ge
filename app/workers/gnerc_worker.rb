7b76cdc08d5ef7c06287d7a918e1b606bfcadb1f
T <opit@gol.ge>
2016-11-21 14:52:42 +0400

    gnerc fix

--- app/workers/gnerc_worker.rb
+++ app/workers/gnerc_worker.rb
@@ -8,16 +8,18 @@ class GnercWorker
 
   	if func == "appeal"
   		stage = 1
-  		doc = clazz.new(parameters)
+  		newcust = clazz.new(parameters)
   	else
   		stage = 2
   		newcust = clazz.where(letter_number: parameters["letter_number"]).first
-  		newcust.update_attributes!(parameters.except("letter_number"))
+  		newcust.update_attributes!(parameters.except("letter_number")) if newcust.present?
   	end
-  	newcust.stage = stage
-  	newcust.save!
-  	queue = Gnerc::SendQueue.new(service: service, service_id: newcust.id, stage: stage, created_at: Time.now)
-  	queue.save!
+    if newcust.present?
+  	  newcust.stage = stage
+  	  newcust.save!
+      queue = Gnerc::SendQueue.new(service: service, service_id: newcust.id, stage: stage, created_at: Time.now)
+      queue.save!
+    end
   end
 
 end
