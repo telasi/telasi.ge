2368223ac51eca9a022e50aaa027aad06a6b616e
T <opit@gol.ge>
2017-06-09 15:45:00 +0400

    customer_cns

--- app/models/billing/customer_cns.rb
+++ app/models/billing/customer_cns.rb
@@ -1,5 +1,7 @@
 # -*- encoding : utf-8 -*-
 class Billing::CustomerCns< ActiveRecord::Base
+  self.establish_connection :bs
+
   self.table_name  = 'bs.customer_cns'
   self.set_integer_columns :status
 end
