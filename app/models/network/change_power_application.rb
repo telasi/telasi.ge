7b1a81b555478829eac900b667b5a895810cf9e9
T <opit@gol.ge>
2017-12-21 11:46:28 +0400

    change power category fix

--- app/models/network/change_power_application.rb
+++ app/models/network/change_power_application.rb
@@ -452,12 +452,12 @@ class Network::ChangePowerApplication
   def get_letter_category
     case self.type
     when TYPE_CHANGE_POWER then 25
-    when TYPE_CHANGE_SOURCE then 31
+    when TYPE_CHANGE_SOURCE then 9
     when TYPE_SPLIT then 27
     when TYPE_RESERVATION then 5
     when TYPE_TEMP_BUILD then 26
     when TYPE_ABONIREBA then 23
-    when TYPE_MICROPOWER then 31
+    when TYPE_MICROPOWER then 23
     when TYPE_SAME_PACK then 25
     when TYPE_HIGH_VOLTAGE then 23
     when TYPE_SUB_CUSTOMER then 28
