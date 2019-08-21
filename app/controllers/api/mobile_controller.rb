c83f9af7a0487aacc8a31056dc837fa89cfc2159
temochutlashvili <opit@gol.ge>
2019-08-21 16:38:35 +0400

    mobile fix

--- app/controllers/api/mobile_controller.rb
+++ app/controllers/api/mobile_controller.rb
@@ -1,24 +1,22 @@
 # -*- encoding : utf-8 -*-
 class Api::NetworkController < Api::ApiController
-  before_action :validate_login
-
   def login
-    render json: { success: true }
+    validate_login
   end
 
   def get_user_info
-    user = Sys::User.authenticate(params[:username].downcase, params[:password])
-    if user and 
-    registration = Customer::Registration.where(user: user).first
-    if registration
-      customer = Billing::Customer.find(registration.custkey)  
-    end
-    if customer
-      render json: { success: true, 
-                     customer_number: customer.accnumb.to_ka, 
-                     customer_name: customer.custname.to_ka } 
-    else 
-      render json: { success: false, message: 'No registration' }
+    validate_loginc do
+      registration = Customer::Registration.where(user: @user).first
+      if registration
+        customer = Billing::Customer.find(registration.custkey)  
+      end
+      if customer
+        render json: { success: true, 
+                       customer_number: customer.accnumb.to_ka, 
+                       customer_name: customer.custname.to_ka } 
+      else 
+        render json: { success: false, message: 'No registration' }
+      end
     end
   end
 
@@ -28,10 +26,11 @@ class Api::NetworkController < Api::ApiController
   private
 
   def validate_login
-    user = Sys::User.authenticate(params[:username].downcase, params[:password]) if params[:username]
-    if user and user.email_confirmed and user.active 
+    @user = Sys::User.authenticate(params[:username].downcase, params[:password]) if params[:username]
+    if @user and @user.email_confirmed and @user.active 
       session[:user_id] = user.id
-    elsif user and not user.email_confirmed then 
+      yield if block_given?
+    elsif @user and not @user.email_confirmed then 
       render json: { success: false, message: I18n.t('model.sys_user.errors.email_not_confirmed') }
     else 
       render json: { success: false, message:  I18n.t('models.sys_user.errors.illegal_login') } 
