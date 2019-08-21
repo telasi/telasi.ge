3a95ce9b518987455d38a224eb284587260ccb6f
temochutlashvili <opit@gol.ge>
2019-08-21 16:28:00 +0400

    mobile login

--- app/controllers/api/mobile_controller.rb
+++ app/controllers/api/mobile_controller.rb
@@ -1,19 +1,15 @@
 # -*- encoding : utf-8 -*-
 class Api::NetworkController < Api::ApiController
+  before_action :validate_login
+
   def login
-    user = Sys::User.authenticate(params[:email].downcase, params[:password]) if params[:email]
-    if user and user.email_confirmed and user.active 
-      session[:user_id] = user.id
-      render json: { success: true }
-    elsif user and not user.email_confirmed then 
-      render json: { success: false, message: I18n.t('model.sys_user.errors.email_not_confirmed') }
-    else 
-      render json: { success: false, message:  I18n.t('models.sys_user.errors.illegal_login') } 
-    end
+    render json: { success: true }
   end
 
   def get_user_info
-    registration = Customer::Registration.where(user: current_user).first
+    user = Sys::User.authenticate(params[:username].downcase, params[:password])
+    if user and 
+    registration = Customer::Registration.where(user: user).first
     if registration
       customer = Billing::Customer.find(registration.custkey)  
     end
@@ -27,5 +23,18 @@ class Api::NetworkController < Api::ApiController
   end
 
   def debts
+  end
+
+  private
+
+  def validate_login
+    user = Sys::User.authenticate(params[:username].downcase, params[:password]) if params[:username]
+    if user and user.email_confirmed and user.active 
+      session[:user_id] = user.id
+    elsif user and not user.email_confirmed then 
+      render json: { success: false, message: I18n.t('model.sys_user.errors.email_not_confirmed') }
+    else 
+      render json: { success: false, message:  I18n.t('models.sys_user.errors.illegal_login') } 
+    end
   end
 end
