8719b064644cd981bbf23a836c5e91efc577e825
temochutlashvili <opit@gol.ge>
2019-10-23 16:36:19 +0400

    outage table fix

--- app/models/billing/outage_journal_cust.rb
+++ app/models/billing/outage_journal_cust.rb
@@ -3,5 +3,5 @@ class Billing::OutageJournalCust < ActiveRecord::Base
   self.table_name  = 'bacho.outage_journal_cust'
   self.primary_key = :custkey_customer
 
-  has_one :detail, class_name: 'Billing::OutageJournalDet', foreign_key: :journal_det_id
+  belongs_to :detail, class_name: 'Billing::OutageJournalDet', foreign_key: :journal_det_id
 end
