8a592564842d17b50e66f0a78c48534ca31872cb
temochutlashvili <opit@gol.ge>
2019-10-23 16:24:41 +0400

    outage tables primary keys

--- app/models/billing/outage_journal_cust.rb
+++ app/models/billing/outage_journal_cust.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 class Billing::OutageJournalCust < ActiveRecord::Base
   self.table_name  = 'bacho.outage_journal_cust'
+  self.primary_key = 'custkey_customer'
 
   has_one :detail, class_name: 'Billing::OutageJournalDet', foreign_key: :journal_det_id
 end
