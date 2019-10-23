e7be463bde3d8dd0cc34209414002e81dd1502bd
temochutlashvili <opit@gol.ge>
2019-10-23 09:39:19 +0400

    cut_history fix

--- app/models/billing/cut_history.rb
+++ app/models/billing/cut_history.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Billing::CutHistory < ActiveRecord::Base
-  self.establish_connection :region_call
+  self.establish_connection :report_bs
 
   self.table_name  = 'bs.cut_history'
   self.set_integer_columns :mark_code, :oper_code, :mark_code_insp, :upload_status, :upload_numb
