7436ff4dcc42e4228f50736ca173a72ec0adb72a
temochutlashvili <opit@gol.ge>
2020-03-01 00:43:18 +0400

    gnerc external api fix

--- app/controllers/api/network_controller.rb
+++ app/controllers/api/network_controller.rb
@@ -2,8 +2,8 @@
 class Api::NetworkController < Api::ApiController
   def newcustomer_sms
   	application = Network::NewCustomerApplication.where(number: params[:number]).first
-    #application.gnerc_id = "G0010#{params[:case_id]}F000#{params[:gnerc_id]}"
-    application.gnerc_id = "G0010#{params[:case_id]}F000#{params[:response_id]}"
+    application.gnerc_id = "G0010#{params[:case_id]}F000#{params[:gnerc_id]}"
+    # application.gnerc_id = "G0010#{params[:case_id]}F000#{params[:response_id]}"
     application.save
     application.first_sms
     render json: { success: true }
@@ -11,8 +11,8 @@ class Api::NetworkController < Api::ApiController
 
   def changepower_sms
   	application = Network::ChangePowerApplication.where(number: params[:number]).first
-    #application.gnerc_id = "G0010#{params[:case_id]}F000#{params[:gnerc_id]}"
-    application.gnerc_id = "G0010#{params[:case_id]}F000#{params[:response_id]}"
+    application.gnerc_id = "G0010#{params[:case_id]}F000#{params[:gnerc_id]}"
+    # application.gnerc_id = "G0010#{params[:case_id]}F000#{params[:response_id]}"
     application.save
     application.first_sms
     render json: { success: true }
