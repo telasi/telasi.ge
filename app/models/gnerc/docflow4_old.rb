02d7271468d76103e20bc1c1273f5f96eb508f9c
temochutlashvili <opit@gol.ge>
2020-03-02 16:44:22 +0400

    gnerc docflow fix

--- app/models/gnerc/docflow4_old.rb
+++ app/models/gnerc/docflow4_old.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
-class Gnerc::Docflow4 < ActiveRecord::Base
+class Gnerc::Docflow4Old < ActiveRecord::Base
   establish_connection :gnerc_old
   self.table_name  = 'semek.docflow4'
   self.set_integer_columns :confirm, :affirmative, :mediate, :letter_category, :stage
