0cff0942cf05a41b212cbf7c885092cad59cf3a1
temochutlashvili <opit@gol.ge>
2020-07-06 15:15:32 +0400

    gnerc fix

--- app/models/gnerc/meter_setup.rb
+++ app/models/gnerc/meter_setup.rb
@@ -2,5 +2,5 @@
 class Gnerc::MeterSetup < ActiveRecord::Base
   establish_connection :gnerc
   self.table_name  = 'metersetup'
-  self.set_integer_columns :confirmation, :building, :abonent_amount, :stage, :response_id
+  self.set_integer_columns :confirmation, :building, :abonent_amount, :stage, :request_status, :response_id
 end
