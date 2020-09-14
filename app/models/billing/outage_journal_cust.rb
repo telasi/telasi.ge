bc71d8ff5a5847dbaedb0da7e09b81ede73d06b9
temochutlashvili <opit@gol.ge>
2020-09-14 12:01:33 +0400

    customer status fix

--- app/models/billing/outage_journal_cust.rb
+++ app/models/billing/outage_journal_cust.rb
@@ -6,6 +6,6 @@ class Billing::OutageJournalCust < ActiveRecord::Base
   belongs_to :detail, class_name: 'Billing::OutageJournalDet', foreign_key: :journal_det_id
   belongs_to :journal, class_name: 'Billing::OutageJournal', foreign_key: :off_id, primary_key: :off_id
 
-  scope :accepted, ->() { joins(:journal).where("OFF_DTIME > TO_DATE('2020-01-01', 'YYYY-MM-DD') AND IS_GIS IN ('ki', 'ara')") }
+  scope :accepted, ->() { joins(:journal).where("OFF_DTIME BETWEEN TO_DATE('2020-01-01', 'YYYY-MM-DD') AND SYSDATE AND IS_GIS IN ('ki', 'ara') AND GADAIDO <> 1") }
   scope :open, ->() { joins(:detail).where('outage_journal_det.ON_TIME is NULL') }
 end
