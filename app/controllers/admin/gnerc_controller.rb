ed6c4793ade762f585c6f42809a31c6b38963b7e
temochutlashvili <opit@gol.ge>
2020-11-20 11:22:15 +0400

    gnerc controller fix

--- app/controllers/admin/gnerc_controller.rb
+++ app/controllers/admin/gnerc_controller.rb
@@ -12,19 +12,24 @@ class Admin::GnercController < ApplicationController
 
   def check
     parameters = params[:postdata]
-    file_key = params[:@parameters][":file_param"]
-    file_param = params[:@parameters][":file"]
-    file = Sys::File.find(file_param)
+    if params[:@parameters]
+      file_key = params[:@parameters][":file_param"]
+      file_param = params[:@parameters][":file"]
+      file = Sys::File.find(file_param)
 
-    content = File.read(file.file.file.file)
-    content = Base64.encode64(content)
+      content = File.read(file.file.file.file)
+      content = Base64.encode64(content)
 
-    parameters[file_key.to_sym] = content
-    parameters["#{file_key}_filename".to_sym] = file.file.filename
+      parameters[file_key.to_sym] = content
+      parameters["#{file_key}_filename".to_sym] = file.file.filename
+    end
 
     GnercWorker.perform_async(params[:stage] == '2' ? "answer" : "appeal", params[:service].to_i, parameters)
+
+    redirect_to admin_gnerc_url
   end
 
   def realsend
+
   end
 end
