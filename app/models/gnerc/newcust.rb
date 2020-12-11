c1f744a8cc5fceb1fe5c0481857acffd8365d296
temochutlashvili <opit@gol.ge>
2020-12-11 11:22:55 +0400

    newcust customer type id

--- app/models/gnerc/newcust.rb
+++ app/models/gnerc/newcust.rb
@@ -2,5 +2,5 @@
 class Gnerc::Newcust < ActiveRecord::Base
   establish_connection :gnerc
   self.table_name  = 'newcust'
-  self.set_integer_columns :confirmation, :building, :abonent_amount, :stage, :response_id
+  self.set_integer_columns :confirmation, :building, :abonent_amount, :stage, :response_id, :customer_type_id
 end
